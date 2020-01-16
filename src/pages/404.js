8d31386a7c74723742cb87965cc43c7b118581e4
Safdar Jamal <safdar.dev@gmail.com>
2020-01-16 22:29:29 +0500

    Add head in 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import Head from '../components/head';
 
 const NotFound = () => {
   return (
     <Layout>
+      <Head title="404" />
       <h1>Page not found</h1>
       <p>Oops! The page you are looking for has been removed or relocated.</p>
       <p>
