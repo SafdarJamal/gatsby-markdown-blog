4463544c56ecd5b04adf17e610cf223be4217085
Safdar Jamal <safdar.dev@gmail.com>
2020-01-16 22:28:36 +0500

    Add head in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import Head from '../components/head';
 
 const Index = () => {
   return (
     <Layout>
+      <Head title="Home" />
       <h1>Home</h1>
       <p>I'm Safdar, a full-stack developer.</p>
     </Layout>
