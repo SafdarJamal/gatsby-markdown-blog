498015d042a8917e147b37880bd59156e202034b
Safdar Jamal <safdar.dev@gmail.com>
2020-01-16 22:32:04 +0500

    Add head in blog template

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
+import Head from '../components/head';
 
 export const query = graphql`
   query($slug: String!) {
@@ -18,6 +19,7 @@ export const query = graphql`
 const Blog = props => {
   return (
     <Layout>
+      <Head title={props.data.markdownRemark.frontmatter.title} />
       <h1>{props.data.markdownRemark.frontmatter.title}</h1>
       <p>{props.data.markdownRemark.frontmatter.date}</p>
       <div
