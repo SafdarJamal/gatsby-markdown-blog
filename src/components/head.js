6d3237097358fc6de5da61b8cdc36bea507bc5b2
Safdar Jamal <safdar.dev@gmail.com>
2020-01-16 22:19:28 +0500

    Create head component

---
+++ src/components/head.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
+import { Helmet } from 'react-helmet';
+
+const Head = ({ title }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  return <Helmet title={`${title} | ${data.site.siteMetadata.title}`} />;
+};
+
+export default Head;
