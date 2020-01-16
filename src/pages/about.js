0348d86a8ceaa601b7dc4d80c6b562ba457c1adb
Safdar Jamal <safdar.dev@gmail.com>
2020-01-16 22:28:51 +0500

    Add head in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import Head from '../components/head';
 
 const About = () => {
   return (
     <Layout>
+      <Head title="About" />
       <h1>About</h1>
       <p>
         <Link to="/contact">Want to work with me? Reach out.</Link>
