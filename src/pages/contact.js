99b50dc7318be822aa6415e872ac69bac392197a
Safdar Jamal <safdar.dev@gmail.com>
2020-01-16 22:29:18 +0500

    Add head in contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import Head from '../components/head';
 
 const Contact = () => {
   return (
     <Layout>
+      <Head title="Contact" />
       <h1>Contact</h1>
       <p>
         The best way to reach me is via{' '}
